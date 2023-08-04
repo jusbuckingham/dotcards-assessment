86608d8eb8c34b26265db7a7b22a21aed2918e4b
jusbuckingham <jus.buckingham@gmail.com>
2023-08-04 12:52:42 -0700

    it compiles

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,43 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { buildSchema } from './database';
+import sqlite3 from 'sqlite3';
+import { open, Database } from 'sqlite';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const DATABASE_FILE = 'database.db'; // Change this to the desired database file name
 
+// In-memory "database"
+let data: Record<string, any>[] = [];
+
+// Helper function to find an item by id
+function findItemById(collection: string, id: string): Record<string, any> | undefined {
+  return data.find((item) => item.id === id && item.collection === collection);
+}
+
+// Function to initialize the database schema
+async function buildSchema() {
+  const db = await open({
+    filename: DATABASE_FILE,
+    driver: sqlite3.Database,
+  });
+
+  // Implement the logic to read JSON schema files and create tables/add columns here
+  // For this example, we will create a simple "items" table.
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS items (
+      id TEXT PRIMARY KEY,
+      collection TEXT,
+      data JSON
+    )
+  `);
+
+  console.log('Database schema initialized.');
+}
+
+// Middleware to initialize the database schema
 app.use(async (req, res, next) => {
   try {
-    await buildSchema();
+    await buildSchema(); // Initialize the database schema based on JSON schema files
     next();
   } catch (err: any) {
     console.error('Error initializing database schema:', err.message);
@@ -15,14 +46,6 @@ app.use(async (req, res, next) => {
 });
 
 app.use(express.json());
-
-// In-memory "database"
-const data: Record<string, any>[] = [];
-
-// Helper function to find an item by id
-function findItemById(collection: string, id: string): Record<string, any> | undefined {
-  return data.find((item) => item.id === id && item.collection === collection);
-}
 
 // API endpoints
 // POST /:collection
@@ -71,13 +94,17 @@ app.delete('/:collection/:id', (req, res) => {
   }
 });
 
+// Error handling for unhandled promise rejections
 process.on('unhandledRejection', (err) => {
   console.error('Unhandled Promise Rejection:', err);
 });
 
+// Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error('Error:', err.stack);
   res.status(500).json({ error: 'Internal Server Error' });
 });
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
